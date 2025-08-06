a7b960789b7d0e3bad11de2f1e7d19948f3c2974
Asim Nazeer <asimnazeer925@gamil.com>
2025-08-06 14:35:42 +0500

    image

--- app/components/VantaClouds.tsx
+++ app/components/VantaClouds.tsx
@@ -16,15 +16,16 @@ export default function VantaClouds() {
           const vanta = await import("vanta/dist/vanta.clouds.min");
           CLOUDS = vanta.default;
         }
-
         const effect = CLOUDS({
           el: vantaRef.current!,
           THREE: THREE,
-          skyColor: 0x0d0d25, // Very dark blue/purple sky
-          cloudColor: 0x444444, // Dark gray clouds
-          cloudShadowColor: 0x000000, // Black shadows
-          sunColor: 0xff6600, // Dim warm sun (orange)
-          speed: 0.1,
+          skyColor: 0x477789, // Almost pitch black sky
+          cloudColor: 0x828ea0, // Very dark gray clouds
+          cloudShadowColor: 0x183550, // True black shadows
+          sunColor: 0xedb875,
+          sunGlareColor: 0xf0eeee, // No visible sun
+          sunlightColor: 0x171818, // Disables any warm light
+          speed: 1, // Very slow and heavy
           mouseControls: true,
           touchControls: true,
         });
